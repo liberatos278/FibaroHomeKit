7f959f293da2df4be2d827f78d142cb4c7f3a428
Liberatos <ja@nectu.email>
2024-05-31 16:02:53 +0200

    blinds

--- api/blinds.js
+++ api/blinds.js
@@ -3,24 +3,29 @@ const apiUri =
   "https://home.fibaro.com/newProxyLite?user=<user>&hc=<hc>&temp=<temp>&req=<actionUri>"
 const actionUri = "/api/devices/:deviceId/action/:action"
 
-async function closeGateway({ accessToken, temp, hc, user }) {
+async function setSlats({ accessToken, temp, hc, user }, position) {
   const uri = apiUri
     .replace("<user>", user)
     .replace("<hc>", hc)
     .replace("<temp>", temp)
     .replace(
       "<actionUri>",
-      actionUri.replace(":deviceId", deviceId).replace(":action", "close")
+      actionUri.replace(":deviceId", deviceId).replace(":action", "setValue2")
     )
+
+  const formData = new URLSearchParams()
+  formData.append("args", "[50]")
 
   await fetch(uri, {
     method: "POST",
     headers: {
       "X-Fibaro-Auth": `RA-Access-Token ${accessToken}`,
+      "Content-Type": "application/x-www-form-urlencoded",
     },
+    body: `{"args":[${position}]}`,
   })
 }
 
 module.exports = {
-  closeGateway,
+  setSlats,
 }
--- api/index.js
+++ api/index.js
@@ -1,21 +1,36 @@
 const express = require("express")
 const app = express()
 
-const { closeGateway } = require("./gateway")
+const { setSlats } = require("./blinds")
 const { login, isSecure } = require("./auth")
 
 require("dotenv").config({ path: __dirname + "/../.env" })
 
-app.get("/gateway", async (req, res) => {
+app.get("/blinds-level", async (req, res) => {
   if (!isSecure(req)) return res.status(401).send("Unauthorized")
 
   const session = await login()
   const action = req.query.action ?? "close"
 
   switch (action) {
+    case "open":
+      await setSlats(session, 100)
+      res.send("Blinds level set to 100%")
+      break
     case "close":
-      await closeGateway(session)
-      res.send("Gateway is closed")
+      await setSlats(session, 0)
+      res.send("Blinds level set to 0%")
+      break
+    case "custom":
+      let position = Number(req.query.position)
+
+      if (!position || isNaN(position))
+        return res.status(400).send("Position is required")
+      if (position > 100) position = 100
+      if (position < 0) position = 0
+
+      await setSlats(session, position)
+      res.send(`Blinds level set to ${position}%`)
       break
     default:
       res.status(400).send("Invalid action")
